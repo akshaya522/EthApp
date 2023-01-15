afd3d1c7e943217fce4800ce7d74e92496ea56b2
Akshaya Muthu <akshaya522@gmail.com>
2023-01-16 02:30:46 +0800

    Add block card

--- src/app/block-card/block-card.component.ts
+++ src/app/block-card/block-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { Block } from '../eth-models';
+import { Web3Service } from '../web3-service';
 
 @Component({
   selector: 'app-block-card',
@@ -6,4 +8,13 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./block-card.component.scss']
 })
 export class BlockCardComponent {
+  @Input() block!: Block;
+
+  constructor(
+    private web3Service : Web3Service
+  ){}
+
+  ngOnInit(): void {
+    console.log("blockkk ", this.block);
+  }
 }
